518d137d181cbe4158bca40b029857676f4317d9
Fudy <lifudong1@163.com>
2022-12-11 16:56:53 +0800

    init

---
+++ src/main/java/com/fudy/itemdetail/FudyShopItemDetailApplication.java
@@ -0,0 +1,13 @@
+package com.fudy.itemdetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudyShopItemDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FudyShopItemDetailApplication.class, args);
+	}
+
+}
