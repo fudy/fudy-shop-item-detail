637f993b8e9473de911c0dba7ede9da0fac90a24
Fudy <lifudong1@163.com>
2023-09-03 22:31:53 +0800

    docker deploy refactor

---
+++ deploy/bin/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+## define variables
+TARGET_DIR=/opt/docker/fudy-shop-item-detail
+TARGET_JAR=../../fudy-shop-item-detail-main/target/fudy-shop-item-detail.jar
---
+++ deploy/bin/restart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname "$0")
+source $SCRIPT_DIR/env.sh
+cd $TARGET_DIR
+docker compose  restart
